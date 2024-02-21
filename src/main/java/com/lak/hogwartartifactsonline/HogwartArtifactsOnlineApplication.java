a4ffcc1fdc05b3a4631d31036eb135fb0dff3b7e
MahmoudNassif <Mahmoud.nassif@igrc2.com>
2024-02-21 17:48:43 +0200

    this is the first commit.

---
+++ src/main/java/com/lak/hogwartartifactsonline/HogwartArtifactsOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.lak.hogwartartifactsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartArtifactsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartArtifactsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lak/hogwartartifactsonline/HogwartArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lak.hogwartartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
